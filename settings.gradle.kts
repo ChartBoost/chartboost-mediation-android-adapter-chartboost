7551b3dac99c5342b44b513fd221b44e14bf9b4c
chauduyphanvu <vu.chau@chartboost.com>
2023-01-20 15:44:07 -0500

    Handle .kts files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
+// Copyright 2022-2023 Chartboost, Inc.
+// 
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
