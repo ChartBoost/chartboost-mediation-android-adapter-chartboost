cd5e1253644083e7bbbc779dbb3da8c4eb97578c
Vu <vu.chau@chartboost.com>
2023-01-20 18:48:36 -0500

    Chartboost: Copyright header (#25)
    * Add copyright header
    
    * Handle .kts files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+// Copyright 2022-2023 Chartboost, Inc.
+// 
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
