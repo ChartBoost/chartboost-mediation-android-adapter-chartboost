57fd85b17edaea8bc15c94c43c848fa8860f2816
chauduyphanvu <vu.chau@chartboost.com>
2023-01-24 10:55:37 -0500

    Redo copyright header comment style

--- ChartboostAdapter/build.gradle.kts
+++ ChartboostAdapter/build.gradle.kts
@@ -1,7 +1,9 @@
-// Copyright 2022-2023 Chartboost, Inc.
-// 
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
+/*
+ * Copyright 2022-2023 Chartboost, Inc.
+ * 
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file.
+ */
 
 import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
 
