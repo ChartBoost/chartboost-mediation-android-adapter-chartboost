e1397e76dd3885d5503ec0d222cf5def5c10eb1c
bichenwang <bichenwang@gmail.com>
2023-02-06 14:38:51 -0800

    [HB-5024] Rename to Chartboost Mediation (#33)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,11 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "7.3.0" apply false
-    id("com.android.library") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("com.android.application") version "7.4.1" apply false
+    id("com.android.library") version "7.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 
-    kotlin("plugin.serialization") version "1.7.22"
+    kotlin("plugin.serialization") version "1.8.10"
 }
 
 task<Delete>("clean") {
