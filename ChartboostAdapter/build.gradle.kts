5e5c6adfe8da88e9b04bfe727ece35e397954235
Bichen Wang <bichenwang@gmail.com>
2023-02-06 12:28:30 -0800

    delete extra dependency

--- ChartboostAdapter/build.gradle.kts
+++ ChartboostAdapter/build.gradle.kts
@@ -63,7 +63,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:+")
     "localImplementation"(project(":Helium"))
 
     // For external usage, please use the following production dependency.
