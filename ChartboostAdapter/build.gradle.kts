6b411efe436b09630981f1b28c728dbc1b772992
Jose Padilla <jose.padilla@chartboost.com>
2023-08-14 09:44:49 -0700

    [HB-6277] Chartboost 9.4.1 - Bumped Chartboost to 9.4.1 - Updated CHANGELOG, README, and Adapter version to 4.9.4.1.0

--- ChartboostAdapter/build.gradle.kts
+++ ChartboostAdapter/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.9.4.0.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.9.4.1.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_CHARTBOOST_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         consumerProguardFiles("proguard-rules.pro")
@@ -74,7 +74,7 @@ dependencies {
 
     // For external usage, please use the following production dependency.
     // You may choose a different release version.
-    implementation("com.chartboost:chartboost-sdk:9.4.0")
+    implementation("com.chartboost:chartboost-sdk:9.4.1")
 
     // Partner SDK Dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
