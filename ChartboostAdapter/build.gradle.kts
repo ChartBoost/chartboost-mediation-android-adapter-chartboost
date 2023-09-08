371d5155b06751aa9b483de392cb20f773b2e151
CB-RyanMcCormick <122290337+CB-RyanMcCormick@users.noreply.github.com>
2023-09-08 09:47:41 -0500

    Update version

--- ChartboostAdapter/build.gradle.kts
+++ ChartboostAdapter/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.9.4.1.1"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.9.4.1.2"
         buildConfigField("String", "CHARTBOOST_MEDIATION_CHARTBOOST_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         consumerProguardFiles("proguard-rules.pro")
