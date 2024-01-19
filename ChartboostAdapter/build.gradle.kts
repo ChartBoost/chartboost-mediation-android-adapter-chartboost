f2f24e23dad6882ca3eb0a0fa7ca2b80da294562
vu <vu.chau@chartboost.com>
2024-01-19 11:28:31 -0500

    Copyright 2024

--- ChartboostAdapter/build.gradle.kts
+++ ChartboostAdapter/build.gradle.kts
@@ -1,6 +1,6 @@
 /*
- * Copyright 2022-2023 Chartboost, Inc.
- *
+ * Copyright 2022-2024 Chartboost, Inc.
+ * 
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file.
  */
