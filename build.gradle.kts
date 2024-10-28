0ea605eaaeaddccce90116d21ac1627eca30fe28
Ray Graham <rayg@bitwisedesign.com>
2024-10-28 10:47:11 -0700

    Updated Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,9 @@ buildscript {
 plugins {
     id("com.android.application") version "7.4.1" apply false
     id("com.android.library") version "7.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
 
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("plugin.serialization") version "1.9.21"
 }
 
 task<Delete>("clean") {
