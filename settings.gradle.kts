cc0195de862058f53b97671af052bc9bdd71d767
chauduyphanvu <vu.chau@chartboost.com>
2025-07-16 12:27:39 -0400

    2025 (#78)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 /*
- * Copyright 2022-2024 Chartboost, Inc.
- *
+ * Copyright 2022-2025 Chartboost, Inc.
+ * 
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file.
  */
