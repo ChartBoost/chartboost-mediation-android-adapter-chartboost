c4b029f93f36f2d69c8b4ff6152e812b5269a3cf
Ray Graham <rayg@bitwisedesign.com>
2026-01-26 09:35:26 -0800

    Update copyright headers to 2026 (#95)
    Update copyright headers to 2026.

--- ChartboostAdapter/src/main/java/com/chartboost/mediation/chartboostadapter/ChartboostAdapter.kt
+++ ChartboostAdapter/src/main/java/com/chartboost/mediation/chartboostadapter/ChartboostAdapter.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 Chartboost, Inc.
+ * Copyright 2023-2026 Chartboost, Inc.
  * 
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file.
