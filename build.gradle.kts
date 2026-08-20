8e15913cd9e66d517e01454100d4e40ba1d010bc
Yonathan Redda <yonathan.redda@loopme.com>
2026-08-20 10:35:51 -0700

    bump kotlin to 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,9 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 
-    kotlin("plugin.serialization") version "2.1.0"
+    kotlin("plugin.serialization") version "2.3.0"
 }
 
 task<Delete>("clean") {
